a4b22251621f1fa60938ed503e83addf4abad7fc
Jakub Jelinek <jakub@redhat.com>
2016-01-14 16:16:30 +0100

    Make sure sum isn't reduced just within each team, but across teams too. Make the testcases also OpenMP 4.5 compliant by adding map(tofrom: sum), otherwise sum is firstprivate on target.

--- sources/Example_teams.4c.c
+++ sources/Example_teams.4c.c
@@ -10,8 +10,8 @@ float dotprod(float B[], float C[])
 {
     float sum = 0;
     int i;
-    #pragma omp target map(to: B[0:N], C[0:N])
-    #pragma omp teams num_teams(8) thread_limit(16)
+    #pragma omp target map(to: B[0:N], C[0:N]) map(tofrom: sum)
+    #pragma omp teams num_teams(8) thread_limit(16) reduction(+:sum)
     #pragma omp distribute parallel for reduction(+:sum) \
                 dist_schedule(static, 1024) schedule(static, 64)
     for (i=0; i<N; i++)
